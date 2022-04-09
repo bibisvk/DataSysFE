225f43c6df3a5ac00c9d533fc6f7f9d573b288ce
mfulajtar <mfulajtar@umb.sk>
2022-04-09 13:28:55 +0200

    customers services a upravenie model + c.ZOZNAM

--- src/app/models/customer.model.ts
+++ src/app/models/customer.model.ts
@@ -14,3 +14,20 @@ export class Customer {
 
   }
 }
+export class CustomerZoznam {
+  customer_id?: number;
+  identification_number: string;
+  customer_firstname: string;
+  customer_lastname: string;
+  o_number: string;
+
+
+  constructor(customer: CustomerZoznam) {
+    this.customer_id = customer.customer_id;
+    this.identification_number = customer.identification_number;
+    this.customer_firstname = customer.customer_firstname;
+    this.customer_lastname = customer.customer_lastname;
+    this.o_number = customer.o_number;
+
+  }
+}
