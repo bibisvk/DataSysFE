38d0b5b97e22096897038acb1bc193194d46cd1f
mfulajtar <mfulajtar@umb.sk>
2022-04-09 13:39:31 +0200

    customer formular

---
+++ src/app/customers-stranka/customers-stranka.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-stranka',
+  templateUrl: './customers-stranka.component.html',
+  styleUrls: ['./customers-stranka.component.css']
+})
+export class CustomersStrankaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
