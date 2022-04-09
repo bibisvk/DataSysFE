ee6306bba0c10cf59264727c9c08b1f666aa6e92
mfulajtar <mfulajtar@umb.sk>
2022-04-09 13:54:17 +0200

    customer stranka

--- src/app/customers-stranka/customers-stranka.component.ts
+++ src/app/customers-stranka/customers-stranka.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer, CustomerZoznam} from "../models/customer.model";
+import {Router} from "@angular/router";
+import {CustomerServiceService} from "../../customer-service.service";
 
 @Component({
-  selector: 'app-customers-stranka',
+  selector: 'app-customer-stranka',
   templateUrl: './customers-stranka.component.html',
   styleUrls: ['./customers-stranka.component.css']
 })
 export class CustomersStrankaComponent implements OnInit {
 
-  constructor() { }
+  customery: CustomerZoznam[] = [];
+
+  customerNaUpravu?: Customer;
+
+  constructor(private router: Router, private customerService: CustomerServiceService) { }
 
   ngOnInit(): void {
+    this.refreshCustomerov();
+  }
+
+  refreshCustomerov(): void {
+    this.customerService.getCustomers().subscribe(data => {
+      console.log('prislo:', data);
+      this.customery = data;
+    });
+  }
+
+  exportCustomerov(): void{
+    this.customerService.exportCustomer().subscribe(x =>{
+      const blob = new Blob([x], {type: 'application/pdf'});
+      const data = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = data;
+      link.download = 'customers.pdf';
+      link.dispatchEvent(new MouseEvent('click'));
+    });
+  }
+
+  chodSpat(): void {
+    this.router.navigate(['']);
+  }
+
+  pridaj(customer: Customer): void {
+    this.customerService.createCustomer(customer).subscribe(data => {
+      console.log('prislo:', data);
+      this.refreshCustomerov();
+    });
+  }
+  uprav(customer: Customer): void {
+    if (customer.customer_id !== undefined) {
+      this.customerService.updateCustomer(customer.customer_id, customer).subscribe(data => {
+        console.log('prislo:', data);
+        this.refreshCustomerov();
+      });
+    }
+  }
+
+  upravZoZoznamu(customerId: number): void {
+    this.customerService.getCustomer(customerId).subscribe(data => {
+      console.log('prislo:', data);
+      this.customerNaUpravu = data;
+    });
+  }
+
+  zmazZoZoznamu(customerId: number): void {
+    this.customerService.deleteCustomer(customerId).subscribe(data => {
+      this.refreshCustomerov();
+    });
   }
 
 }
